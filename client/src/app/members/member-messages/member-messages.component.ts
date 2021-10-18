99e1ac39d73a7680ed0daf6399a61edb2a57f935
Dong Tran <dongtran5295@gmail.com>
2021-10-18 23:52:53 +0700

    Section 19 - pre switch to postgres

--- client/src/app/members/member-messages/member-messages.component.ts
+++ client/src/app/members/member-messages/member-messages.component.ts
@@ -7,7 +7,7 @@ import { MessageService } from 'src/app/_services/message.service';
   changeDetection: ChangeDetectionStrategy.OnPush,
   selector: 'app-member-messages',
   templateUrl: './member-messages.component.html',
-  styleUrls: ['./member-messages.component.css']
+  styleUrls: ['./member-messages.component.css'],
 })
 export class MemberMessagesComponent implements OnInit {
   @ViewChild('messageForm') messageForm!: NgForm;
